bf59b521580911f1ba18ecaa18fbaccf96f1b3e8
gowrishankarb220-ship-it <gowrishankarb220@gmail.com>
2026-07-21 14:50:25 +0530

    Create traversal.java

---
+++ traversal.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String[] args) {
+        int[] marks={70,60,70,90,30,40,68};
+        String[] name={"aravind","arun","anbu","askar","gokul","harish","vel"};
+        for(int i=0;i<marks.length;i++){
+             System.out.println(name[i]+"="+marks[i]);
+    }
+}
+}
